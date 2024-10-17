f6b7ec1f65e10b9073e9e88a1ad31ad0410dae9e
aradhya684 <aradhyaambole92@gmail.com>
2024-10-17 11:04:26 +0530

    17-10-2024

--- super_pet/products/urls.py
+++ super_pet/products/urls.py
@@ -8,7 +8,10 @@ urlpatterns =[
     path('drools/',views.drools,name="droolspage"),  
     path('whiskas/',views.whiskas,name="whiskaspage"),
     path("search",views.search,name="search"),
-    path('<int:pk>/',views.ProductDetailView.as_view(),name="product_detail")
+    path('<int:pk>/',views.ProductDetailView.as_view(),name="product_detail"),
+    path("create-product/",views.ProductCreateView.as_view(), name="create-product"),
+    path("update-product/<int:pk>/",views.ProductUpdateView.as_view(),name="update-product"),
+    path("delete-product/<int:pk>/",views.ProductDeleteView.as_view(),name="delete-product")
 
 ]
 
--- super_pet/products/views.py
+++ super_pet/products/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from .models import Product
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 
 
 # Create your views here.
 #this is function based views
 def products(request):
     return render(request,"products.html")
-
 
 def drools(request):
     p1=Product.cust_manager.drools()
@@ -22,16 +21,28 @@ def search(request):
     product = Product.cust_manager.all().filter(product_name__icontains = keyword)
     return render(request,"products.html", {"products":product})
 
+#this is list view import above
 class ProductListView(ListView):
-    model = Product #keep this variable this is default variable
+    model = Product #keep this variable this is default variable, here Product is our model name 
 
-
+#this is detail view import above
 class ProductDetailView(DetailView):
-    model = Product #keep this variable this is default variable
+    model = Product #keep this variable this is default variable, here Product is our model name  
     template_name = 'products/productdetails.html'
 
+#this is create view import above
 
-    
+class ProductCreateView(CreateView):
+    model = Product
+    fields="__all__"
+    success_url = "/products"
 
+ #this is update view import above
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields="__all__"
+    success_url = "/products"
 
-
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = "/products"
